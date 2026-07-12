efb48933f15197979f007404046415cfb90568a0
Chudasama Vishal <118047155+VishalChudasama08@users.noreply.github.com>
2026-07-12 23:21:49 +0530

    create spring boot initial project & database configuration

---
+++ securevaultx-spring-boot/src/main/java/com/securevaultx/backend/SecurevaultxSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.securevaultx.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurevaultxSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurevaultxSpringBootApplication.class, args);
+	}
+
+}
---
+++ securevaultx-spring-boot/src/test/java/com/securevaultx/backend/SecurevaultxSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securevaultx.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurevaultxSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
